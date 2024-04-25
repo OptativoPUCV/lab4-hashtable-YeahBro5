c15f9b5c60b2879e0984c7f862b0d1c6a67553db
YeahBro5 <147212074+YeahBro5@users.noreply.github.com>
2024-04-25 03:42:05 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -59,7 +59,7 @@ void insertMap(HashMap * map, char * key, void * value) {
     while (map->buckets[posicion] != NULL)
       {
         posicion++;
-        if (posicion == map->capacity) posicion = 0;
+        if (posicion == map->size) posicion = 0;
       }
     
     map->buckets[posicion] = newPair;
