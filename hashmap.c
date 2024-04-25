442bae9bac8f43704de5d8008f64201b6433b032
YeahBro5 <147212074+YeahBro5@users.noreply.github.com>
2024-04-25 02:30:17 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -40,6 +40,11 @@ int is_equal(void* key1, void* key2){
 
 
 void insertMap(HashMap * map, char * key, void * value) {
+  if (map == NULL || key ==  NULL)
+  {
+    return;
+  }
+  
   long posicion = hash(key, map->capacity);
   
   map->buckets[posicion] = createPair(key, value);
