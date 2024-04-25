e983a4fe24d342945f30961cb26aa10f97a52bf2
YeahBro5 <147212074+YeahBro5@users.noreply.github.com>
2024-04-25 17:23:11 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -155,7 +155,7 @@ Pair * firstMap(HashMap * map) {
   if (map == NULL || map->size < 0) return NULL; 
   for (long i = 0; i <= map->capacity;i++)
     {
-      if (map->buckets[i]->key != NULL) return map->buckets[i];
+      if (map->buckets[i] != NULL && map->buckets[i]->key != NULL) return map->buckets[i];
     }
   
 
