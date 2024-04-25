dcffd152b36677fa17dbe33ef11e6601b0451c86
YeahBro5 <147212074+YeahBro5@users.noreply.github.com>
2024-04-25 17:17:42 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -152,6 +152,12 @@ Pair * searchMap(HashMap * map,  char * key)
 
 
 Pair * firstMap(HashMap * map) {
+  if (map == NULL || map->size <= 0) return NULL; 
+  for (long i = 0; i <= map->capacity;i++)
+    {
+      if (map->buckets[i]->key != NULL) return map->buckets[i];
+    }
+  
 
     return NULL;
 }
